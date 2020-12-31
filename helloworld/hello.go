3d191f822923df9858b5b4be95abd29f972e898d
markkizz <mark.pattanapara@gmail.com>
2020-12-31 10:59:19 +0700

    init golang tut project

---
+++ helloworld/hello.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	// "example.com/greetings"
+)
+
+func main() {
+	var num int
+	num = 123
+	fmt.Println(num)
+	fmt.Printf("hello\n")
+	// msg := callHello()
+	// fmt.Println(msg)
+}
+
+// continous type para
+func multiple(num1, num2 int) int {
+	return num1 * num2
+}
+
+// func callHello() string {
+// 	return greetings.Hello("mark")
+// }
+
+// type struct markky {
+// 	name string
+// 	lastname string
+// }
